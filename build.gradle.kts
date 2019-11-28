92bc8b62f5a3b857928cd8cf32b448aec1819115
iPotato <monkeydpotato@gmail.com>
2019-11-28 17:09:35 +0800

    upload useless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.gradle.api.JavaVersion.VERSION_1_8
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    maven
     // kotlin
     val kotlinVersion = "1.3.50"
     kotlin("jvm") version kotlinVersion
@@ -14,8 +13,6 @@ java.sourceCompatibility = VERSION_1_8
 
 dependencies {
     implementation(gradleApi())
-    // tools
-    implementation("com.monkeydp:tools")
     // test
     testImplementation("junit:junit:4.12")
 }
@@ -24,13 +21,5 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = VERSION_1_8.toString()
-    }
-}
-
-tasks.named<Upload>("uploadArchives") {
-    repositories.withGroovyBuilder {
-        "mavenDeployer" {
-            "repository"("url" to uri("../../ignore/plugins-repo"))
-        }
     }
 }
