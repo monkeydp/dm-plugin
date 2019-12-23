cd7f31f9c9e342d466c30ab8ca52ee8fb898aa83
iPotato <monkeydpotato@gmail.com>
2019-12-24 01:00:54 +0800

    add Delegates.singletonOrNull(..)

--- src/main/kotlin/com/monkeydp/daios/dm/plugin/ext/Project.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/plugin/ext/Project.kt
@@ -3,7 +3,7 @@ package com.monkeydp.daios.dm.plugin.ext
 import com.monkeydp.daios.dm.plugin.DmPluginExt
 import com.monkeydp.daios.dm.plugin.config.projectConfig
 import com.monkeydp.tools.ext.kotlin.initInstance
-import com.monkeydp.tools.ext.kotlin.notNullSingleton
+import com.monkeydp.tools.ext.kotlin.singleton
 import org.gradle.api.Project
 import kotlin.properties.Delegates
 
@@ -11,7 +11,7 @@ import kotlin.properties.Delegates
  * @author iPotato
  * @date 2019/11/29
  */
-internal var Project.dmPluginExt by Delegates.notNullSingleton<DmPluginExt>(ignoreAlreadyInitialized = true)
+internal var Project.dmPluginExt by Delegates.singleton<DmPluginExt>(ignoreAlreadyInitialized = true)
 
 fun Project.dmPluginExt(
         @Suppress("unused_parameter")
